b166b753df387904e7a7524b16b0ed14e377c88b
Dhaval <71810466+Dhavalgj2@users.noreply.github.com>
2023-04-07 23:40:27 +1000

    changes

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,7 +1,58 @@
 import React from "react";
-
+import { useDispatch, useSelector } from "react-redux";
+import { changeName, changeCost, addCar } from "../store";
 const CarForm = () => {
-  return <div>CarForm</div>;
+  const { name, cost } = useSelector((state) => {
+    return {
+      name: state.form.name,
+      cost: state.form.cost,
+    };
+  });
+
+  const dispatch = useDispatch();
+
+  const handleNameChange = (e) => {
+    dispatch(changeName(e.target.value));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addCar({ name, cost }));
+    dispatch(changeCost(0));
+    dispatch(changeName(""));
+  };
+  const handleCostChange = (e) => {
+    const carCost = parseInt(e.target.value) || 0;
+    dispatch(changeCost(carCost));
+  };
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form onSubmit={handleSubmit}>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+
+          <div className="field">
+            <label className="label">Cost</label>
+            <input
+              className="input is-expanded"
+              value={cost || ""}
+              onChange={handleCostChange}
+            />
+          </div>
+        </div>
+        <div className="field">
+          <button className="button is-link">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CarForm;
--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeCar } from "../store";
 
 const CarList = () => {
-  return <div>CarList</div>;
+  const dispatch = useDispatch();
+  const cars = useSelector(({ form, cars: { cars, searchTerm } }) => {
+    return cars.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
+
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car));
+  };
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - $ {car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car.id)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 };
 
 export default CarList;
